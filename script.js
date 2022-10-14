e4c214a0107563e3ce3d51f3d84ca58abfba286a
kappyllo <33810546+kappyllo@users.noreply.github.com>
2022-10-14 07:34:39 +0200

    Adding css futures, adding js dom's

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const btn__7 = document.querySelector(".btn__7");
+const btn__8 = document.querySelector(".btn__8");
+const btn__9 = document.querySelector(".btn__9");
+const btn__devide = document.querySelector(".btn__devide");
+const btn__4 = document.querySelector(".btn__4");
+const btn__5 = document.querySelector(".btn__5");
+const btn__6 = document.querySelector(".btn__6");
+const btn__multiply = document.querySelector(".btn__multiply");
+const btn__1 = document.querySelector(".btn__1");
+const btn__2 = document.querySelector(".btn__2");
+const btn__3 = document.querySelector(".btn__3");
+const btn__0 = document.querySelector(".btn__0");
+const btn__equal = document.querySelector(".btn__equal");
